14f80ed797f307fde3ca4b4e65d88074aab18548
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-27 17:54:36 -0300

    add validators

--- src/app/Components/credit-card-form/credit-card-form.component.ts
+++ src/app/Components/credit-card-form/credit-card-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-credit-card-form',
@@ -8,7 +8,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CreditCardFormComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl('')
+    name: new FormControl('', [Validators.required, Validators.minLength(3)])
   })
   constructor() { }
 
