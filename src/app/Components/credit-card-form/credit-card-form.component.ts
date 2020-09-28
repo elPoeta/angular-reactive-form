ff869cd451dac6e4ba359946fc43a132ac564dde
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-28 20:13:35 -0300

    complete form control properties

--- src/app/Components/credit-card-form/credit-card-form.component.ts
+++ src/app/Components/credit-card-form/credit-card-form.component.ts
@@ -8,7 +8,11 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CreditCardFormComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)])
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    cardNumber: new FormControl('', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]),
+    expiration: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9] | 1[0-2])\/\d{2}$/)]),
+    securityCode: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)])
+
   })
   constructor() { }
 
