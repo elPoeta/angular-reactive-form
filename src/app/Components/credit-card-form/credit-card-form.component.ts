9f99cd84565fd971b9a3f3afa2d26b088bebd371
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-28 20:25:04 -0300

    fix patern regex add submit button

--- src/app/Components/credit-card-form/credit-card-form.component.ts
+++ src/app/Components/credit-card-form/credit-card-form.component.ts
@@ -10,13 +10,17 @@ export class CreditCardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     cardNumber: new FormControl('', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]),
-    expiration: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9] | 1[0-2])\/\d{2}$/)]),
+    expiration: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
     securityCode: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)])
 
   })
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+
   }
 
 }
